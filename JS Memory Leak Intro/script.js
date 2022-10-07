07ed1effb977f4b2abf79cd1b67989745fd2b842
Sintu Singh <sintu.singh@Sintus-MacBook-Pro.local>
2022-10-07 07:18:04 -0400

    JS Memory

--- JS Memory Leak Intro/script.js
+++ JS Memory Leak Intro/script.js
@@ -1,46 +1,9 @@
 const btn = document.getElementById('create-leak');
 btn.onclick = createArrays;
-
 function createArrays() {
     const N = 100000;
-
-    window.emptyArr = new Array(N)
-
-    window.strArr = []
+    window.helloList = []
     for (var i = 0; i < N; i++) {
-        var str = (Math.random() * 10000000000).toString().substr(0, 10)
-        window.strArr.push(str)
+        window.helloList.push(`Hello-${i}`)
     }
-
-    window.sameStrArr = []
-    for (var i = 0; i < N; i++) {
-        window.sameStrArr.push("Hello")
-    }
-
-    window.numArr = []
-    for (var i = 0; i < N; i++) {
-        window.numArr.push(Math.random())
-    }
-
-    window.objArr = []
-    for (var i = 0; i < N; i++) {
-        window.objArr.push({})
-    }
-
-    window.boolArr = []
-    for (var i = 0; i < N; i++) {
-        window.boolArr.push(true)
-    }
-
-    window.arrayArr = []
-    for (var i = 0; i < N; i++) {
-        window.arrayArr.push([])
-    }
-
-    window.mixedArr = []
-    for (var i = 0; i < N; i++) {
-        window.mixedArr.push(false)
-    }
-    window.mixedArr.shift()
-    window.mixedArr.push({})
 }
